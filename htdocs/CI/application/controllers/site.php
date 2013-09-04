ba6eed67cb9f03163005ef455e5fff8a0cb1b997
Ravishankar.S <srshankar145@gmail.com>
2013-09-04 06:21:51 +0530

    Codeignitor Framework
    Codeignitor Framework full setup

---
+++ htdocs/CI/application/controllers/site.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class site extends MY1_Controller {
+
+       function index() {
+	         echo 'hello world';
+	   }
+	   
+	   function dosomething()  {
+	         echo 'do something';
+	   }
+}
